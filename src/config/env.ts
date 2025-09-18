af4cb4444eb9b30707f053459cd870fb2052d3c9
commited_dev <devcommited@gmail.com>
2025-09-18 09:46:52 +0200

    add env var

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,5 +1,5 @@
 import { config } from "dotenv";
 
-config({ path: `.env.${process.env.NODE_ENV || "development.local"}` });
+config({ path: `.env.${process.env.NODE_ENV || "develop.local"}` });
 
 export const { PORT, SERVER_URL, NODE_ENV } = process.env;
