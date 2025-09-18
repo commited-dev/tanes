d84e86c1f7bdf30539491e47d0d1b22177df6739
commited_dev <devcommited@gmail.com>
2025-09-18 09:59:24 +0200

    create a mongodb connection

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,4 +2,4 @@ import { config } from "dotenv";
 
 config({ path: `.env.${process.env.NODE_ENV || "develop.local"}` });
 
-export const { PORT, SERVER_URL, NODE_ENV } = process.env;
+export const { PORT, SERVER_URL, NODE_ENV, MONGODB_URI } = process.env;
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import app from "./app.ts";
 import { PORT } from "./config/env.ts";
+import { connectDB } from "./config/mongobd.ts";
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`🚀 Server running at http://localhost:${PORT}`);
+
+  await connectDB();
 });
