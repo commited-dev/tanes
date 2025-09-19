944243e24d102c5aa8da82fd359ae79694a2c213
commited_dev <devcommited@gmail.com>
2025-09-19 11:29:56 +0200

    create a swagger script

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express from "express";
 
 import authRouter from "./routes/auth.routes.js";
+import { setupSwagger } from "./swagger.js";
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/api/v1/auth", authRouter);
+
+setupSwagger(app);
 
 export default app;
