95cc38f7bbb8f6ed72e5c22e4cb51f30bf0fd836
commited_dev <devcommited@gmail.com>
2025-09-20 09:48:41 +0200

    create a route taht will fetch account for the authentificated user

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,11 +1,19 @@
 import { Router } from "express";
-import { login, logout, register } from "../controllers/auth.controller.js";
+import {
+  getMyAccount,
+  login,
+  logout,
+  register,
+} from "../controllers/auth.controller.js";
+import { authenticateUser } from "../middlewares/auth.middleware.js";
 
 const authRouter = Router();
 
 authRouter.post("/login", login);
 
 authRouter.post("/register", register);
+
+authRouter.get("/my-account", authenticateUser, getMyAccount);
 
 authRouter.post("/logout", logout);
 
